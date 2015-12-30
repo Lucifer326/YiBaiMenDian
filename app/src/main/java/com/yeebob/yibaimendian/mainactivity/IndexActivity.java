07f6d9f3b2bf546e84225b19762412e9a6035a38
wgl <wanggl@zhiqingsoft.com>
2015-12-30 18:49:10 +0800

    全屏无titlebar

--- app/src/main/java/com/yeebob/yibaimendian/mainactivity/IndexActivity.java
+++ app/src/main/java/com/yeebob/yibaimendian/mainactivity/IndexActivity.java
@@ -2,10 +2,16 @@ package com.yeebob.yibaimendian.mainactivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.yeebob.yibaimendian.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 /**
@@ -15,9 +21,63 @@ import org.xutils.x;
 @ContentView(R.layout.indexactivity)
 public class IndexActivity extends AppCompatActivity {
 
+    @ViewInject(R.id.tool_bar)
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
+
+        // App Logo
+        mToolbar.setLogo(R.mipmap.ic_launcher);
+        // Title
+        mToolbar.setTitle("翼百门店");
+        // Sub Title
+        mToolbar.setSubtitle("go go go");
+        setSupportActionBar(mToolbar);
+        // 设置导航栏图标
+        mToolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        // 菜单事件监听
+        // Menu item click 的監聽事件一樣要設定在 setSupportActionBar 才有作用
+        mToolbar.setOnMenuItemClickListener(onMenuItemClick);
+        //返回按钮
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    msg += "Click edit";
+                    break;
+                case R.id.action_share:
+                    msg += "Click share";
+                    break;
+                case R.id.action_settings:
+                    msg += "Click setting";
+                    break;
+            }
+
+            if (!msg.equals("")) {
+                Toast.makeText(IndexActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 為了讓 Toolbar 的 Menu 有作用，這邊的程式不可以拿掉
+        getMenuInflater().inflate(R.menu.menu_index, menu);
+        return true;
+       // return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/yeebob/yibaimendian/mainactivity/LoginActivity.java
+++ app/src/main/java/com/yeebob/yibaimendian/mainactivity/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 import com.yeebob.yibaimendian.R;
@@ -28,8 +27,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //全屏显示
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         x.view().inject(this);
         //打开新页面
         SubmitButton.setOnClickListener(new View.OnClickListener() {
