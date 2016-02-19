05e32b8eabe4b6f1fad10f07da1ec5dfa0fe5eda
Lucifer326 <99752193@qq.com>
2016-02-19 18:29:21 +0800

    HttpUtils

---
+++ app/src/main/java/com/yeebob/yibaimendian/utils/HttpUtils.java
@@ -0,0 +1,12 @@
+package com.yeebob.yibaimendian.utils;
+
+/**
+ * 网络请求工具类
+ * Created by WGl on 2016-1-14.
+ */
+public class HttpUtils {
+    //服务器基准地址
+    //public final static String BASEURL = "http://iwshop.yeebob.com/?/";
+    public final static String BASEURL = "http://iwshopapi.yebob.net/?/";
+}
+  
